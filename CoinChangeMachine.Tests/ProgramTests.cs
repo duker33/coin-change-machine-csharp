5ab8676093e1e26a7bc16b8b91b4d0f2b2d367c4
Mark Shevchenko <m.shevchenko@taxys.ru>
2018-09-14 14:22:21 +0300

    Реализовал алгоритм и тесты

---
+++ CoinChangeMachine.Tests/ProgramTests.cs
@@ -0,0 +1,80 @@
+namespace CoinChangeMachine.Tests
+{
+    using System.IO;
+    using System.Linq;
+    using Xunit;
+
+    public class ProgramTests
+    {
+        [Fact]
+        public void MakeChange_WithImpossibleSum_ReturnsEmptyArray()
+        {
+            var actual = Program.MakeChange(new[] {(5, 2), (3, 2)}, 4);
+
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void MakeChange_WithPossibleSum_ReturnsMaximalValueCoinsFirst()
+        {
+            var actual = Program.MakeChange(new[] { (5, 2), (2, 5) }, 10);
+
+            Assert.Equal(new[] {(5, 2)}, actual);
+        }
+
+        [Fact]
+        public void ReadCoins_WithZeroN_ReturnsEmptyEnumerable()
+        {
+            using (var reader = new StringReader("0"))
+            {
+                var coins = Program.ReadCoins(reader)
+                                   .ToArray();
+
+                Assert.Empty(coins);
+            }
+        }
+
+        [Fact]
+        public void ReadCoins_WithPositiveN_ReturnsNElements()
+        {
+            using (var reader = new StringReader("3\n1 100\n2 200\n3 300"))
+            {
+                var coins = Program.ReadCoins(reader)
+                                   .ToArray();
+
+                Assert.Equal(1, coins[0].value);
+                Assert.Equal(100, coins[0].count);
+
+                Assert.Equal(2, coins[1].value);
+                Assert.Equal(200, coins[1].count);
+
+                Assert.Equal(3, coins[2].value);
+                Assert.Equal(300, coins[2].count);
+            }
+        }
+
+        [Fact]
+        public void WriteCoins_WithEmptyCollection_PrintsZero()
+        {
+            using (var writer = new StringWriter())
+            {
+                writer.NewLine = "\n";
+                Program.WriteCoins(new (int value, int count)[0], writer);
+
+                Assert.Equal("0\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void WriteCoins_WithNonEmptyCollection_PrintsPairs()
+        {
+            using (var writer = new StringWriter())
+            {
+                writer.NewLine = "\n";
+                Program.WriteCoins(new[] {(1, 100), (2, 200), (3, 300)}, writer);
+
+                Assert.Equal("1 100\n2 200\n3 300\n", writer.ToString());
+            }
+        }
+    }
+}
---
+++ CoinChangeMachine/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace CoinChangeMachine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var coins = ReadCoins(Console.In);
+            var sum = int.Parse(Console.ReadLine());
+            var change = MakeChange(coins, sum);
+            WriteCoins(change, Console.Out);
+        }
+
+        public static IReadOnlyCollection<(int value, int count)> MakeChange(IEnumerable<(int value, int count)> coins, int sum)
+        {
+            var sortedCoins = coins.OrderByDescending(x => x.value);
+            var result = new List<(int value, int count)>();
+
+            using (var enumerator = sortedCoins.GetEnumerator())
+            {
+                while (enumerator.MoveNext() && sum > 0)
+                {
+                    var value = enumerator.Current.value;
+                    var count = Math.Max(sum / value, enumerator.Current.count);
+                    sum -= value * count;
+
+                    if (sum < 0)
+                        return new (int value, int count)[0];
+
+                    result.Add((value, count));
+                }
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<(int value, int count)> ReadCoins(TextReader reader)
+        {
+            var n = int.Parse(reader.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var coin = reader.ReadLine()
+                                 .Split(' ');
+
+                Debug.Assert(coin.Length == 2);
+                var value = int.Parse(coin[0]);
+                var count = int.Parse(coin[1]);
+
+                yield return (value, count);
+            }
+        }
+
+        public static void WriteCoins(IReadOnlyCollection<(int value, int count)> coins, TextWriter writer)
+        {
+            if (coins.Count > 0)
+            {
+                foreach (var coin in coins)
+                    writer.WriteLine("{0} {1}", coin.value, coin.count);
+            }
+            else
+                writer.WriteLine("0");
+        }
+    }
+}
